3bbe4a5a8af4988b010246694a81b888abbcca0c
csny <masaono_13@msn.com>
2014-09-13 14:28:47 +0900

    再アップ

--- cgrect/MyDrawView.m
+++ cgrect/MyDrawView.m
@@ -8,6 +8,7 @@
 
 #import "MyDrawView.h"
 
+BOOL isGreen[7][10]; // 方眼のタッチ有無を格納する配列
 @implementation MyDrawView
 
 
@@ -16,6 +17,12 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor whiteColor];
+    }
+    // 配列初期化
+    for(int i=0;i<7;i++){
+        for(int j=0;j<10;j++){
+            isGreen[i][j]=NO;
+        }
     }
     return self;
 }
@@ -26,8 +33,7 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     
     // 色を定義
-    CGFloat red[4] = {1.0f, 0.0f, 0.0f, 1.0f};
-    CGFloat blue[4] = {0.0f, 0.0f, 1.0f, 1.0f};
+    CGFloat white[4] = {0.0f, 0.0f, 0.0f, 0.0f};
     CGFloat cyan[4] = {0.5f, 0.8f, 1.0f, 1.0f};
     CGFloat green[4] = {0.0f, 1.0f, 0.0f, 1.0f};
     
@@ -45,26 +51,44 @@
     }
     CGContextStrokePath(context);
     
-    // 矩形を描画
-    CGContextSetFillColor(context, green);
-    CGContextFillRect(context, CGRectMake(50, 50, 50, 50));
-    
-    // 円を描画
-    CGContextSetFillColor(context, blue);
-    CGContextFillEllipseInRect(context, CGRectMake(100, 100, 50, 100));
-    
-    // 多角形を描画
-    CGContextSetFillColor(context, red);
-    CGContextBeginPath(context);
-    CGContextMoveToPoint(context, 200, 150);
-    CGContextAddLineToPoint(context, 250, 200);
-    CGContextAddLineToPoint(context, 250, 100);
-    CGContextFillPath(context);
+    // フラグに応じて矩形を描画、イベントで一部再読み込みされる
+    for(int i=0;i<7;i++){
+        for(int j=0;j<10;j++){
+            if(isGreen[i][j]==YES){
+                CGContextSetFillColor(context, green);
+                CGContextFillRect(context, CGRectMake(i*50,j*50,50,50));
+            }else{
+                CGContextSetFillColor(context, white);
+                CGContextFillRect(context, CGRectMake(i*50,j*50,50,50));
+            }
+            
+        }
+    }
     
     // テキストを描画
     UIFont *font = [UIFont fontWithName:@"AvenirNext-Italic" size:20.0f];
     [[UIColor colorWithRed:.8f green:0 blue:0 alpha:1.0f] set];
-    [@"hogehoge" drawAtPoint:CGPointMake(50, 300) withAttributes:@{NSFontAttributeName:font}];
+    [@"touch any square" drawAtPoint:CGPointMake(50, 300) withAttributes:@{NSFontAttributeName:font}];
+}
+
+// タッチイベント
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    // タッチ位置を点で取得
+    CGPoint pt = [[touches anyObject] locationInView:self];
+    for(int i=0;i<7;i++){
+        for(int j=0;j<10;j++){
+            // タッチ位置と方眼位置を比較
+            if (CGRectContainsPoint(CGRectMake(i*50,j*50,50,50),pt)) {
+                if(isGreen[i][j]==NO){
+                    isGreen[i][j]=YES;
+                }else{
+                    isGreen[i][j]=NO;
+                }
+                // 範囲を限定してCGRectを再読み込・再描画
+                [self setNeedsDisplayInRect:CGRectMake(i*50,j*50,50,50)];
+            }
+        }
+    }
 }
 
 @end
